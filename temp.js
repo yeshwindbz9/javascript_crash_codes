dce758dc9d4a757898f336b7cf656d95a21cf9e8
yeshwin <64612584+yeshwindbz9@users.noreply.github.com>
2021-08-02 21:22:51 +0530

    temp

---
+++ temp.js
@@ -0,0 +1,11 @@
+(function(){
+    "use strict";
+    /* Start of your code */
+    function greetMe(yourName) {
+      alert('Hello ' + yourName);
+    }
+  
+    greetMe('World');
+    /* End of your code */
+  })();
+  
